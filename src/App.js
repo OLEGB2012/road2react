83a719f1d5e4f5184ec1bb9db6193118f50e987b
OlegB <olegb2012@gmail.com>
2022-04-07 03:34:45 +0300

    React JSX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+const welcome = {
+  greeting: 'Hey',
+  title: 'React',
+};
+
+function getProjectName(name) {
+  return name;
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>{welcome.greeting} {welcome.title}</h1>
+      <h2>{getProjectName('Hacker Stories')}</h2>
+
+      <label htmlFor="search">Search:</label>
+      <input id='search' type="text"/>
     </div>
   );
 }
