f2f16f299f1233d7cab7094cd2e4368e6e78a4b9
OlegB <olegb2012@gmail.com>
2022-04-16 04:46:17 +0300

    Switch from native fetch api to axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+// > npm install axios
+import axios from "axios";
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
@@ -57,12 +59,12 @@ const App = () => {
 
   const handleFetchStories = React.useCallback(() => {
     dispatchStories({type: 'STORIES_FETCH_INIT'});
-    fetch(url)
-      .then(response => response.json())
+    axios
+      .get(url)
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
       .catch(() =>
