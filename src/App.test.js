d9d248b8a062b3ad9319a5a8d15fc0367724823d
OlegB <olegb2012@gmail.com>
2022-06-03 01:57:32 +0300

    Add some testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,128 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/* eslint-disable testing-library/await-async-query */
+import React from "react";
+import renderer from "react-test-renderer";
+import App, { Item, List, SearchForm, InputWithLabel } from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("Item", () => {
+  const item = {
+    title: "React",
+    url: "https://reactjs.org",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  };
+  const handleRemoveItem = jest.fn();
+  let component;
+  beforeEach(() => {
+    component = renderer.create(
+      <Item item={item} onRemoveItem={handleRemoveItem} />
+    );
+  });
+
+  it("render all properties", () => {
+    expect(component.root.findByType("a").props.href).toEqual(
+      "https://reactjs.org"
+    );
+
+    // https://github.com/facebook/react/issues/16962
+    // Вставить .filter(x => typeof (x.type) === 'string'), чтобы спрятать стелс узлы.
+    // Баг работы findAllByProps в сочетании с styled-components
+    expect(
+      component.root
+        .findAllByProps({ children: "Jordan Walke" })
+        .filter((x) => typeof x.type === "string").length
+    ).toEqual(1);
+  });
+
+  it("calls onRemoveItem on button click", () => {
+    component.root.findByType("button").props.onClick();
+    expect(handleRemoveItem).toHaveBeenCalledTimes(1);
+    expect(handleRemoveItem).toHaveBeenCalledWith(item);
+    expect(component.root.findAllByType(Item).length).toEqual(1);
+  });
+});
+
+describe("List", () => {
+  const list = [
+    {
+      title: "React",
+      url: "https://reactjs.org",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  it("renders two items", () => {
+    const component = renderer.create(<List list={list} />);
+    expect(component.root.findAllByType(Item).length).toEqual(2);
+  });
+});
+
+describe("SearchForm", () => {
+  const searchFormProps = {
+    searchTerm: "React",
+    onSearchInput: jest.fn(),
+    onSearchSubmit: jest.fn(),
+  };
+  let component;
+  beforeEach(() => {
+    component = renderer.create(<SearchForm {...searchFormProps} />);
+  });
+  it("renders the input field with its value", function () {
+    const value = component.root.findByType("input").props.value;
+    expect(value).toEqual("React");
+  });
+  it("changes the input field", () => {
+    const pseudoEvent = { target: "Redux" };
+    component.root.findByType("input").props.onChange(pseudoEvent);
+    expect(searchFormProps.onSearchInput).toHaveBeenCalledTimes(1);
+    expect(searchFormProps.onSearchInput).toHaveBeenCalledWith(pseudoEvent);
+  });
+  it("submit the form", () => {
+    const pseudoEvent = {};
+    component.root.findByType("form").props.onSubmit(pseudoEvent);
+    expect(searchFormProps.onSearchSubmit).toHaveBeenCalledTimes(1);
+    expect(searchFormProps.onSearchSubmit).toHaveBeenCalledWith(pseudoEvent);
+  });
+  it("disables the button and prevents submit", () => {
+    component.update(<SearchForm {...searchFormProps} searchTerm="" />);
+    expect(component.root.findByType("button").props.disabled).toBeTruthy();
+  });
+});
+
+describe("App", () => {
+  it("success fetching data with a list", () => {
+    const list = [
+      {
+        title: "React",
+        url: "https://reactjs.org",
+        author: "Jordan Walke",
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+      },
+      {
+        title: "Redux",
+        url: "https://redux.js.org",
+        author: "Dan Abramov, Andrew Clark",
+        num_comments: 2,
+        points: 5,
+        objectID: 1,
+      },
+    ];
+    const component = renderer.create(<App />);
+    // findByType - не работает с List = React.memo(...) ???
+    // expect(component.root.findByType(List).props.list).toEqual(list);
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,53 @@
-import React from 'react';
-// > npm install axios
+import React from "react";
 import axios from "axios";
 // import styles from './App.module.css';
-// > npm install styled-components
-import styled from 'styled-components';
+import styled from "styled-components";
 // @ts-ignore
-import {ReactComponent as Check} from "./check.svg"; // See to fix: https://annacoding.com/article/5I7om6mCrW25KeA4ObeyMJ/Two-ways-to-solve:-Cannot-find-SVG-module-error-when-compiling-Typescript-in-React-application
-
+import { ReactComponent as Check } from "./check.svg"; // See to fix: https://annacoding.com/article/5I7om6mCrW25KeA4ObeyMJ/Two-ways-to-solve:-Cannot-find-SVG-module-error-when-compiling-Typescript-in-React-application
 
 type Story = {
-  objectID: string,
-  url: string,
-  title: string,
-  author: string,
-  num_comments: number,
-  points: number,
-}
+  objectID: string;
+  url: string;
+  title: string;
+  author: string;
+  num_comments: number;
+  points: number;
+};
 
 type ItemProps = {
-  item: Story,
-  onRemoveItem: (item: Story) => void,
-}
+  item: Story;
+  onRemoveItem: (item: Story) => void;
+};
 
 type Stories = Array<Story>;
 
 type ListProps = {
-  list: Stories,
-  onRemoveItem: (item: Story) => void,
-}
+  list: Stories;
+  onRemoveItem: (item: Story) => void;
+};
 
 type StoriesState = {
-  data: Stories,
-  isLoading: boolean,
-  isError: boolean,
-}
+  data: Stories;
+  isLoading: boolean;
+  isError: boolean;
+};
 
 interface StoriesFetchInitAction {
-  type: 'STORIES_FETCH_INIT',
+  type: "STORIES_FETCH_INIT";
 }
 
 interface StoriesFetchSuccessAction {
-  type: 'STORIES_FETCH_SUCCESS',
+  type: "STORIES_FETCH_SUCCESS";
   payload: Stories;
 }
 
 interface StoriesFetchFailureAction {
-  type: 'STORIES_FETCH_FAILURE',
+  type: "STORIES_FETCH_FAILURE";
 }
 
 interface StoriesRemoveAction {
-  type: 'REMOVE_STORY',
-  payload: Story,
+  type: "REMOVE_STORY";
+  payload: Story;
 }
 
 type StoriesAction =
@@ -60,19 +57,19 @@ type StoriesAction =
   | StoriesRemoveAction;
 
 type SearchFormProps = {
-  searchTerm: string,
-  onSearchInput: (event: React.ChangeEvent<HTMLInputElement>) => void,
-  onSearchSubmit: (event: React.FormEvent<HTMLFormElement>) => void,
-}
+  searchTerm: string;
+  onSearchInput: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onSearchSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+};
 
 type InputWithLabelProps = {
-  id: string,
-  value: string,
-  type?: string,
-  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
-  isFocused?: boolean,
-  children: React.ReactNode,
-}
+  id: string;
+  value: string;
+  type?: string;
+  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  isFocused?: boolean;
+  children: React.ReactNode;
+};
 
 const StyledContainer = styled.div`
   height: 100vw;
@@ -96,7 +93,7 @@ const StyledItem = styled.div`
 `;
 
 interface ColumnProps {
-  width?: string,
+  width?: string;
 }
 
 const StyledColumn = styled.span`
@@ -159,8 +156,8 @@ const StyledInput = styled.input`
 `;
 
 interface CheckProps {
-  height?: string,
-  width?: string,
+  height?: string;
+  width?: string;
 }
 
 const StyledCheck = styled(Check)`
@@ -168,35 +165,32 @@ const StyledCheck = styled(Check)`
   width: ${(props: CheckProps) => props.width};
 `;
 
-const storiesReducer = (
-  state: StoriesState,
-  action: StoriesAction
-) => {
+const storiesReducer = (state: StoriesState, action: StoriesAction) => {
   switch (action.type) {
-    case 'STORIES_FETCH_INIT':
+    case "STORIES_FETCH_INIT":
       return {
         ...state,
         isLoading: true,
         isError: false,
       };
-    case 'STORIES_FETCH_SUCCESS':
+    case "STORIES_FETCH_SUCCESS":
       return {
         ...state,
         isLoading: false,
         isError: false,
         data: action.payload,
       };
-    case 'STORIES_FETCH_FAILURE':
+    case "STORIES_FETCH_FAILURE":
       return {
         ...state,
         isLoading: false,
         isError: true,
       };
-    case 'REMOVE_STORY':
+    case "REMOVE_STORY":
       return {
         ...state,
         data: state.data.filter(
-          story => action.payload.objectID !== story.objectID
+          (story) => action.payload.objectID !== story.objectID
         ),
       };
     default:
@@ -204,52 +198,61 @@ const storiesReducer = (
   }
 };
 
-const useSemiPersistentState = (key: string, initialState: string): [string, (newValue: string) => void] => {
+const useSemiPersistentState = (
+  key: string,
+  initialState: string
+): [string, (newValue: string) => void] => {
   const isMounted = React.useRef(false);
-  const [value, setValue] = React.useState(localStorage.getItem(key) || initialState);
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
   React.useEffect(() => {
     if (!isMounted.current) {
       isMounted.current = true;
     } else {
-      console.log('A');
+      console.log("A");
       localStorage.setItem(key, value);
     }
   }, [value, key]);
   return [value, setValue];
 };
 
-const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
 
-const getSumComments = (stories: { isLoading?: boolean; isError?: boolean; data: any; }) => {
-  console.log('C');
+const getSumComments = (stories: {
+  isLoading?: boolean;
+  isError?: boolean;
+  data: any;
+}) => {
+  console.log("C");
 
   return stories.data.reduce(
-    (result: any, value: { num_comments: any; }) => result + value.num_comments, 0
+    (result: any, value: { num_comments: any }) => result + value.num_comments,
+    0
   );
 };
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
+  const [searchTerm, setSearchTerm] = useSemiPersistentState("search", "React");
 
-  const [url, setUrl] = React.useState(
-    `${API_ENDPOINT}${searchTerm}`
-  )
+  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
 
-  const [stories, dispatchStories] = React.useReducer(
-    storiesReducer,
-    {data: [], isLoading: false, isError: false}
-  );
+  const [stories, dispatchStories] = React.useReducer(storiesReducer, {
+    data: [],
+    isLoading: false,
+    isError: false,
+  });
 
   const handleFetchStories = React.useCallback(async () => {
-    dispatchStories({type: 'STORIES_FETCH_INIT'});
+    dispatchStories({ type: "STORIES_FETCH_INIT" });
     try {
       const result = await axios.get(url);
       dispatchStories({
-        type: 'STORIES_FETCH_SUCCESS',
+        type: "STORIES_FETCH_SUCCESS",
         payload: result.data.hits,
       });
     } catch {
-      dispatchStories({type: 'STORIES_FETCH_FAILURE'});
+      dispatchStories({ type: "STORIES_FETCH_FAILURE" });
     }
   }, [url]);
 
@@ -259,7 +262,7 @@ const App = () => {
 
   const handleRemoveStory = React.useCallback((item: Story) => {
     dispatchStories({
-      type: 'REMOVE_STORY',
+      type: "REMOVE_STORY",
       payload: item,
     });
   }, []);
@@ -273,13 +276,15 @@ const App = () => {
     event.preventDefault();
   };
 
-  console.log('B:App');
+  console.log("B:App");
 
-  const sumComments = React.useMemo(() => getSumComments(stories), [stories,]);
+  const sumComments = React.useMemo(() => getSumComments(stories), [stories]);
 
   return (
     <StyledContainer>
-      <StyledHeadlinePrimary>My Hacker Stories with {sumComments} comments</StyledHeadlinePrimary>
+      <StyledHeadlinePrimary>
+        My Hacker Stories with {sumComments} comments
+      </StyledHeadlinePrimary>
 
       <SearchForm
         searchTerm={searchTerm}
@@ -291,18 +296,17 @@ const App = () => {
       {stories.isLoading ? (
         <p>Loading ...</p>
       ) : (
-        <List
-          list={stories.data} onRemoveItem={handleRemoveStory}
-        />)}
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
+      )}
     </StyledContainer>
   );
 };
 
 const SearchForm = ({
-                      searchTerm,
-                      onSearchInput,
-                      onSearchSubmit,
-                    }: SearchFormProps) => (
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit,
+}: SearchFormProps) => (
   <StyledSearchForm onSubmit={onSearchSubmit}>
     <InputWithLabel
       id="search"
@@ -320,14 +324,13 @@ const SearchForm = ({
 );
 
 const InputWithLabel = ({
-                          id,
-                          value,
-                          type = 'text',
-                          onInputChange,
-                          isFocused,
-                          children,
-                        }: InputWithLabelProps) => {
-
+  id,
+  value,
+  type = "text",
+  onInputChange,
+  isFocused,
+  children,
+}: InputWithLabelProps) => {
   const inputRef = React.useRef<HTMLInputElement>(null!);
   React.useEffect(() => {
     if (isFocused && inputRef.current) {
@@ -350,21 +353,15 @@ const InputWithLabel = ({
   );
 };
 
-const List = React.memo(
-  ({list, onRemoveItem}: ListProps) => (
-    <>
-      {list.map(item => (
-        <Item
-          key={item.objectID}
-          item={item}
-          onRemoveItem={onRemoveItem}
-        />
-      ))}
-    </>
-  )
-);
+const List = React.memo(({ list, onRemoveItem }: ListProps) => (
+  <>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+    ))}
+  </>
+));
 
-const Item = ({item, onRemoveItem}: ItemProps) => (
+const Item = ({ item, onRemoveItem }: ItemProps) => (
   <StyledItem>
     <StyledColumn width="40%">
       <a href={item.url}>{item.title}</a>
@@ -374,9 +371,10 @@ const Item = ({item, onRemoveItem}: ItemProps) => (
     <StyledColumn width="10%">{item.points}</StyledColumn>
     <StyledColumn width="10%">
       <StyledButtonSmall type="button" onClick={() => onRemoveItem(item)}>
-        <StyledCheck height="18px" width="18px"/>
+        <StyledCheck height="18px" width="18px" />
       </StyledButtonSmall>
     </StyledColumn>
   </StyledItem>
 );
 export default App;
+export { SearchForm, InputWithLabel, List, Item }; // for accessing components from jest testing engine
